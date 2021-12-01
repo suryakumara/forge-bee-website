02f87e865009284af3abc025e07f01380e72e6cc
Surya Kumara <suryakumara33@gmail.com>
2021-12-01 15:48:53 +0800

    add: switch building

--- src/index.js
+++ src/index.js
@@ -1,22 +1,44 @@
 const myViewerDiv = document.getElementById("forgeViewer");
 let viewer = new Autodesk.Viewing.Private.GuiViewer3D(myViewerDiv);
-const options = {
-  env: "Local",
-  applyScaling: { to: "meters" },
-  globalOffset: { x: 0, y: 0, z: 0 },
-  keepCurrentModels: true,
-  document: "./assets/models/office/beeinventoroffice_3rdfloor.svf",
-};
-Autodesk.Viewing.Initializer(options, function () {
-  viewer = new Autodesk.Viewing.GuiViewer3D(
-    document.getElementById("forgeViewer"),
-    { extensions: ["Autodesk.DocumentBrowser", "BeeInventor"] }
-  );
-  viewer.start();
-  viewer.loadModel(options.document, options);
 
-  setCamera(viewer);
+function initializeViewer(
+  model = "./assets/models/sample/building-example.svf"
+) {
+  const options = {
+    env: "Local",
+    applyScaling: { to: "meters" },
+    globalOffset: { x: 0, y: 0, z: 0 },
+    keepCurrentModels: true,
+    document: model,
+  };
+  Autodesk.Viewing.Initializer(options, function () {
+    viewer = new Autodesk.Viewing.GuiViewer3D(
+      document.getElementById("forgeViewer"),
+      { extensions: ["Autodesk.DocumentBrowser", "BeeInventor"] }
+    );
+    viewer.start();
+    viewer.loadModel(options.document, options);
+
+    setCamera(viewer);
+  });
+}
+initializeViewer();
+const handleModel = document.getElementById("switch-model");
+handleModel.addEventListener("change", () => {
+  if (handleModel.checked) {
+    initializeViewer("./assets/models/sample/building-example.svf");
+    unloadViewer();
+  } else {
+    initializeViewer("./assets/models/office/beeinventoroffice_3floor.svf");
+    unloadViewer();
+  }
 });
+
+function unloadViewer() {
+  viewer.finish();
+  viewer = null;
+  Autodesk.Viewing.shutdown();
+}
 
 function setCamera(viewer) {
   setTimeout(() => {
